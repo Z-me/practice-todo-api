541d128c3c9a99f603c991a9881daf5bcc10611e
Hajime <b1011177@hotmail.co.jp>
2022-04-27 21:30:34 +0900

    add update test

--- test/api/todo_test.go
+++ test/api/todo_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"strconv"
 
 	// "reflect"
 
@@ -68,13 +69,7 @@ func TestCreateItem(t *testing.T) {
 			status:	 	http.StatusNotFound,
 			isError: 	true,
 			payload:	`{"title": "Test TODO", "status": "Done", "details": "test_todo", "priority": "P0"}`,
-			expected:	model.Todo{
-				ID: 		nextID,
-				Title:		"Test TODO",
-				Status:		"Done",
-				Details:	"test_todo",
-				Priority:	"P0",
-			},
+			expected:	model.Todo{},
 			need2Delete: false,
 		},
 		{
@@ -145,6 +140,131 @@ func TestCreateItem(t *testing.T) {
 		})
 	}
 }
+
+func TestUpdateItem(t *testing.T) {
+	// Note: Start test Server
+	ts := httptest.NewServer(api.Router())
+	defer ts.Close()
+
+	// Note: Start Connect DB
+	err := db.ConnectDB()
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	defer db.DisconnectDB()
+
+	// Note: 事前処理
+	target := model.Payload{
+		Title:		"Test TODO",
+		Status:		"Done",
+		Details:	"test_todo",
+		Priority:	"P0",
+	}
+	res, err := db.AddNewTodo(target)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	nextID := res.ID
+
+	cases := []struct{
+		name 		string
+		url			string
+		client		string
+		status 		int
+		isError 	bool
+		payload		string
+		expected	model.Todo
+		need2Delete bool
+	}{
+		{
+			name: 		"正常系: 更新",
+			url: 		"/todo/" + strconv.Itoa(int(nextID)),
+			client: 	"PUT",
+			status:	 	http.StatusCreated,
+			isError: 	false,
+			payload:	`{"title": "Changed TODO", "status": "Done", "details": "changed_todo", "priority": "P0"}`,
+			expected:	model.Todo{
+				ID: 		nextID,
+				Title:		"Changed TODO",
+				Status:		"Done",
+				Details:	"changed_todo",
+				Priority:	"P0",
+			},
+			need2Delete: true,
+		},
+		{
+			name: 		"異常系: 更新: 404",
+			url: 		"/todo/" + strconv.Itoa(int(nextID)),
+			client: 	"POST",
+			status:	 	http.StatusNotFound,
+			isError: 	true,
+			payload:	`{"title": "Changed TODO", "status": "Done", "details": "changed_todo", "priority": "P0"}`,
+			expected:	model.Todo{},
+			need2Delete: false,
+		},
+		{
+			name: 		"異常系: 新規追加: 400",
+			url: 		"/todo/" + strconv.Itoa(int(nextID)),
+			client: 	"PUT",
+			status:	 	http.StatusBadRequest,
+			isError: 	true,
+			payload:	`{"Message": "Bad Request"}`,
+			expected:	model.Todo{},
+			need2Delete: false,
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(caseNameHelper(t, c.name, c.client, c.url), func(t *testing.T) {
+			client := &http.Client{}
+			req, err := http.NewRequest(c.client, ts.URL + c.url, bytes.NewBuffer([]byte(c.payload)))
+			if err != nil {
+				t.Fatalf("Expected no error, got %v", err)
+			}
+
+			res, err := client.Do(req)
+			if err != nil {
+				t.Fatalf("Expected no error, got %v", err)
+			}
+			defer res.Body.Close()
+
+			if res.StatusCode != c.status {
+				t.Fatalf("[New Item] Expected status code %v, got %v", c.status, res.StatusCode)
+			}
+			var resData handler.Todo
+			json.NewDecoder(res.Body).Decode(&resData)
+
+			// CreatedAtなどは比較したくないので除外
+			if !c.isError {
+				if	c.expected.ID != uint(resData.ID) {
+					t.Fatalf("ID: want %v, resData = %v", c.expected.ID, resData.ID)
+				}
+				if c.expected.Title != resData.Title {
+					t.Fatalf("Title: want %v, resData = %v", c.expected.Title, resData.Title)
+				}
+				if c.expected.Status != resData.Status {
+					t.Fatalf("Status: want %v, resData = %v", c.expected.Status, resData.Status)
+				}
+				if c.expected.Details != resData.Details {
+					t.Fatalf("Details: want %v, resData = %v", c.expected.Details, resData.Details)
+				}
+				if c.expected.Priority != resData.Priority {
+					t.Fatalf("Priority: want %v, resData = %v", c.expected.Priority, resData.Priority)
+				}
+			}
+		})
+	}
+	// Note: 事後削除処理
+	err = db.ConnectDB()
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	defer db.DisconnectDB()
+
+	db.DeleteItem(nextID)
+}
+
 /*
 func TestGetTodoList(t *testing.T) {
 	// Note: Start test Server
